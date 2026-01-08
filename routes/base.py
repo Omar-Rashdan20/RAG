2fc584f21a17c3f3ee80735ff12daf24a2bdccda
Omar-Rashdan20 <147641880+Omar-Rashdan20@users.noreply.github.com>
2026-01-08 18:00:10 +0300

    Commit

--- routes/base.py
+++ routes/base.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, APIRouter
+import os
+from dotenv import load_dotenv
+load_dotenv(".env")
 
 base_router=APIRouter(
     prefix="/rag/v01",
     tags=["rag_v01"],
 )
 @base_router.get("/")
-def base_info():
-    return {"message": "RAG API Base Endpoint"}
+async def base_info():
+    app_name = os.getenv("app_name")
+    app_version = os.getenv("app_version")
+    return {"app_name": app_name, 
+            "app_version": app_version}
