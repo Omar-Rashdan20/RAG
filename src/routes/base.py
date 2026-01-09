f5241eff554b0610daa6fe3b33be309d8765b16c
Omar-Rashdan20 <147641880+Omar-Rashdan20@users.noreply.github.com>
2026-01-09 11:48:44 +0300

    commit

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 import os
 from dotenv import load_dotenv
 from helpers.config import settings_loader,Settings
@@ -10,7 +10,7 @@ base_router=APIRouter(
     tags=["rag_v01"],
 )
 @base_router.get("/")
-async def base_info(settings:Settings=depends(settings_loader)):
+async def base_info(settings:Settings=Depends(settings_loader)):
     #settings = settings_loader()
     app_name = settings.app_name
     app_version = settings.app_version
